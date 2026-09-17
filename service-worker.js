65cb7d51263787f9052a7c82ad12daeb2f056eb6
takutaku02 <w98k5ypz78@privaterelay.appleid.com>
2026-09-17 23:00:19 +0900

    ホーム画面用アイコン変更

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_VERSION = "date-money-v2";
+const CACHE_VERSION = "date-money-v3";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/apple-touch-icon.png"
+  "./icons/icon-192.png?v=2",
+  "./icons/icon-512.png?v=2",
+  "./icons/apple-touch-icon.png?v=2"
 ];
 
 self.addEventListener("install", (event) => {
